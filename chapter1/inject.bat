dad76e5ceaac1cf13fc9c7ea38d72ab3c290065a
ruinspecter <omarware1876@gmail.com>
2026-02-24 17:14:30 +0200

    Create inject.bat

---
+++ chapter1/inject.bat
@@ -0,0 +1,23 @@
+@echo off
+echo Copying Chapter 1 save files to DELTARUNE folder...
+echo.
+
+set "target=%LOCALAPPDATA%\DELTARUNE"
+set "source=%~dp0"
+
+if not exist "%target%" (
+    echo DELTARUNE folder not found!
+    echo Make sure the game has been launched at least once.
+    pause
+    exit /b
+)
+
+echo Source: %source%
+echo Target: %target%
+echo.
+
+xcopy "%source%*" "%target%\" /E /I /Y
+
+echo.
+echo Copy complete!
+pause
